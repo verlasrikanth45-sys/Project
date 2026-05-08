40f6a140eca2c7bdca0c4076d5c71d59bb93db96
verlasrikanth45-sys <verlasrikanth45@gmail.com>
2026-05-08 14:41:50 +0530

    Create AboutUs.jsx

---
+++ AboutUs.jsx
@@ -0,0 +1,16 @@
+# Company Details
+
+## Company Name
+ABC Technologies
+
+## About the Company
+ABC Technologies is a software development company specializing in web applications and AI solutions.
+
+## Services
+- Web Development
+- Cloud Computing
+- AI & Machine Learning
+
+## Contact Information
+Email: info@abctech.com
+Website: www.abctech.com
